9c8d8fa840da3572431f90ed0ec7edbc0e20c6f1
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-09-22 22:21:03 -0300

    feat: REcuperando um produto em EditarProduto

--- my-app/src/routes/EditarProdutos/index.tsx
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -1,16 +1,36 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-
-const listaProdutos = [
-    { id: 1, nome: "Produto 1", preco: 10.0 },
-    { id: 2, nome: "Produto 2", preco: 20.0 },
-    { id: 3, nome: "Produto 3", preco: 30.0 },
-];
+import type { TipoProduto } from "../../types/types";
 
 export default function EditarProdutos() {
 
-    const { id } = useParams<string >();
+    const { id } = useParams<{id:string}>();
 
-    const produto = listaProdutos.find( ( p )=> p.id === Number(id));
+    const[produto,setProduto] = useState<TipoProduto>({} as TipoProduto);
+
+    useEffect( ()=>{
+
+        const carregarProduto = async()=>{
+            
+            try {
+                const response = await fetch(`http://localhost:3001/produtos/${id}`);
+    
+                if(!response.ok){
+                    throw new Error("Produto não encontrado!");
+                }
+
+                const data:TipoProduto = await response.json();
+                console.log(data);
+                setProduto(data);
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        carregarProduto();
+    },[]);
+
+    // const produto = listaProdutos.find( ( p )=> p.id === Number(id));
     
     return (
         <main>
